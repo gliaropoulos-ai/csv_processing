e664753500c3bd8404509c2bbdb6b5259cc94bd8
gerryAI <gerry@intelligencia.ai>
2024-01-30 02:47:38 +0200

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as stl
+import pandas as pd
 from csv_preprocessing_functions import to_snake_case, curate_csv_file, input_to_output_csv
 
 stl.title("CSV Preprocessing for banking")
@@ -10,7 +11,7 @@ stl.write("A Computer Science portal for Geeks")
 from io import StringIO
 
 uploaded_file = stl.file_uploader("Upload a CSV file")
+df = pd.read_csv(uploaded_file)
 # output_file = input_to_output_csv(uploaded_file)
-stl.download_button(label= "Download CSV", data = uploaded_file) #, file_name = to_snake_case(uploaded_file))  
-
+stl.download_button(label= "Download CSV", data = df, file_name = to_snake_case(uploaded_file.name), mime="text/csv")
 
