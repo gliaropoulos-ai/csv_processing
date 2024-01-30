be407f300796aa610bab3b3c52c4cf6478da7590
gerryAI <gerry@intelligencia.ai>
2024-01-30 02:39:02 +0200

    str(name)

--- csv_preprocessing_functions.py
+++ csv_preprocessing_functions.py
@@ -2,7 +2,7 @@ import pandas as pd
 import re
 
 def to_snake_case(name):
-    name = name.replace(" ", "_")
+    name = str(name).replace(" ", "_")
     name = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
     name = re.sub('__([A-Z])', r'_\1', name)
     name = re.sub('([a-z0-9])([A-Z])', r'\1_\2', name)
