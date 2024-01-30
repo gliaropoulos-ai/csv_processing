1b4567b7d9af1411146d95627fa28f748e237d13
gerryAI <gerry@intelligencia.ai>
2024-01-30 15:44:40 +0200

    to_excel

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,14 +6,10 @@ stl.title("CSV Preprocessing for banking")
 
 stl.write("A tool to help automate our finance and backoffice services")
 
-
-
-from io import StringIO
-
 uploaded_file = stl.file_uploader("Upload a CSV file" , type=["csv", "xlsx"])
 if uploaded_file:
     stl.write("Filename: ", uploaded_file.name)
     csv_uploaded_file = pd.read_csv(uploaded_file, sep=";", encoding= "ISO-8859-7")
     data_out = curate_csv_file(csv_uploaded_file)
-    stl.download_button(label= "Download CSV", data = data_out.to_csv(index= False, encoding = "utf-8"), file_name = to_snake_case(uploaded_file.name), mime="text/csv")
+    stl.download_button(label= "Download XLSX", data = data_out.to_excel(to_snake_case(uploaded_file.name).replace("csv", "xlsx"), index= False, encoding = "utf-8"), file_name = to_snake_case(uploaded_file.name).replace("csv", "xlsx"), mime="application/vnd.ms-excel")
 
