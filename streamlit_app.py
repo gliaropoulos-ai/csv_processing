73dfce9702e3008c09cdcfc70b015a89a796e7f6
gerryAI <gerry@intelligencia.ai>
2024-01-30 23:04:43 +0200

    to_csv fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,7 @@ stl.title("CSV Preprocessing for banking")
 stl.write("A tool to help automate our finance and backoffice services")
 
 uploaded_file = stl.file_uploader("Upload a CSV file" , type=["csv", "xlsx"])
+
 if uploaded_file:
     stl.write("File to be processed: ", uploaded_file.name)
     csv_uploaded_file = pd.read_csv(uploaded_file, sep=";", encoding= "ISO-8859-7")
@@ -27,4 +28,4 @@ if uploaded_file:
     else:
         csv_name = to_snake_case(uploaded_file.name)
         stl.write("Output File: ", csv_name)
-        stl.download_button(label= "Download CSV", data = data_out.to_csv(csv_name, index= False,encoding="utf-8"), mime="text/csv")
+        stl.download_button(label= "Download CSV", data = data_out.to_csv(index= False,encoding="utf-8"), file_name = csv_name, mime="text/csv")
