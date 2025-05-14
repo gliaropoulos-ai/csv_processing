e95c1b55d7561f07daf8fe4d516398869b82cdb0
gerryAI <gerry@intelligencia.ai>
2025-05-14 14:52:42 +0300

    fix export xlsxl

--- pages/4_Peiraios EXTRAIT Processing.py
+++ pages/4_Peiraios EXTRAIT Processing.py
@@ -6,14 +6,18 @@ from zipfile import ZipFile
 from csv_preprocessing_functions import to_snake_case, read_streamlit_csv_file, write_streamlit_csv_file, curate_csv_file_generic, input_to_output_csv, peiraios_xlsx_preprocessing
 
 
-stl.title("Preprocessing for Alphabank transactions")
+stl.title("Preprocessing for Peiraios transactions")
 
-stl.write("A tool to sync Alphabank transactions with EXTRAIT")
+stl.write("A tool to sync Peiraios transactions with EXTRAIT")
 
 uploaded_file = stl.file_uploader("Upload a XLSX File" , type=["xlsx"])
 if uploaded_file:
     stl.write("File to be processed: ", uploaded_file.name)
-    xlsx_uploaded_file = pd.read_excel(uploaded_file)
-    xlsx_uploaded_file = peiraios_xlsx_preprocessing(xlsx_uploaded_file)
-    stl.write("Output File: ", uploaded_file.name)
-    stl.download_button(label= "Download XLSX", data = xlsx_uploaded_file, file_name =  uploaded_file.name)
+    xlsx_uploaded_df = pd.read_excel(uploaded_file)
+    xlsx_uploaded_df = peiraios_xlsx_preprocessing(xlsx_uploaded_df)
+    xlsx_file_name = uploaded_file.name
+    stl.write("Output XLSX File: ", xlsx_file_name)
+    xlsx_uploaded_df.to_excel(xlsx_file_name, index=False)
+
+    with open(xlsx_file_name, mode="rb") as zf:
+        stl.download_button(label= "Download XLSX", data = zf, file_name = xlsx_file_name)
